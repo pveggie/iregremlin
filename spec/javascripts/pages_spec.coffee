eb5a1b4a1e03676f3af685df595d2260df943df7
Gemma Gotch <pveggie@hotmail.com>
2016-09-22 21:34:29 +0200

    Having trouble getting teaspoon tests to work

---
+++ spec/javascripts/pages_spec.coffee
@@ -0,0 +1,10 @@
+describe "Testing", ->
+  it "is going so smoothly", ->
+    expect(true).toBe true
+
+#   it "is not going so smoothly", ->
+#     expect(true).toBe false
+
+# describe "getCoords", ->
+#   it "returns an array of column number and row number from an object", ->
+#     expect(getCoords $('.ire')).toBe [1,2]
--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -65,7 +65,7 @@ Teaspoon.configure do |config|
     #suite.hook :fixtures, &proc{}
 
     # Determine whether specs loaded into the test harness should be embedded as individual script tags or concatenated
-    # into a single file. Similar to Rails' asset `debug: true` and `config.assets.debug = true` options. By default, 
+    # into a single file. Similar to Rails' asset `debug: true` and `config.assets.debug = true` options. By default,
     # Teaspoon expands all assets to provide more valuable stack traces that reference individual source files.
     #suite.expand_assets = true
 
@@ -98,7 +98,7 @@ Teaspoon.configure do |config|
   # Selenium Webdriver: https://github.com/modeset/teaspoon/wiki/Using-Selenium-WebDriver
   # BrowserStack Webdriver: https://github.com/modeset/teaspoon/wiki/Using-BrowserStack-WebDriver
   # Capybara Webkit: https://github.com/modeset/teaspoon/wiki/Using-Capybara-Webkit
-  #config.driver = :phantomjs
+  config.driver = :phantomjs
 
   # Specify additional options for the driver.
   #
